4dfccefda2c712e7f496461ace83ef3fcc0a3c49
Nishant Dogra <13938655+mrdogra007@users.noreply.github.com>
2019-02-19 17:00:59 +0530

    Pure CSS Card widget
    Pure CSS Card widget - Bootstrap Framework

---
+++ pure-css-cards-widget/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/mrdogra007/pen/jddJpB.
+
+ Pure CSS based Deal and Offer cards widget with label - Bootstrap Framework - its great for the shopping type of sites to show the multiple offers with the description. Re Crafted on Force Framework.
---
+++ pure-css-cards-widget/js/index.js
@@ -0,0 +1,7 @@
+// Pure CSS cards - Bootstrap Framework
+// Checkout the re crafted plugins for Force Framework :: https://codepen.io/mrdogra007/pen/dZGbay
+// Deal and Offer cards for ecommerce websites and stores, side widgets for offer cards 
+// Crafted on: Force Framework
+// Author: Nishant Dogra
+// Twitter: @mrdogra007
+// http://dograsweblog.com/drive/force-framework/
