b87290ab1019f44a07ca7e1b2eb71bd1d3fcf78b
Nishant Dogra <13938655+mrdogra007@users.noreply.github.com>
2019-03-04 23:42:24 +0530

    team management tree plugin - Force Framework
    team management tree plugin - Force Framework

---
+++ team-management-tree-plugin-force-framework/js/index.js
@@ -0,0 +1,7 @@
+// Team Management Tree plugin
+// Simple HTML/CSS Management Tree, Tree Architecture plugin - Force Framework
+// Deigned by Nishant Dogra
+// Crafted: 09/05/2018
+// Updated: 28/02/2019
+// Twitter: @mrdogra007
+// Website: http://dograsweblog.com/drive/force-framework/
