906d09441a2b8a42d289d0604527757310954929
Nishant Dogra <13938655+mrdogra007@users.noreply.github.com>
2019-03-04 23:34:03 +0530

    Files added
    Codepen plugin Files added

---
+++ scroll-container-vertical-slider/js/index.js
@@ -0,0 +1,23 @@
+// < scroll-container > tag: An idea to convert scroll behavior for vertical slider
+// Scrolling behavior - pure CSS work 
+// Deigned by Nishant Dogra
+// Date: 10/09/2018
+// Twitter: @mrdogra007
+// Blog: https://t.co/F6RF2rCHu3
+
+// Benefits:
+// 1. No javascript
+// 2. Pure CSS
+// 3. Image will loan inside CSS, no direct load
+// 4. Light weight
+// 5. Cross browser compatibility
+
+// How to use: 
+// just place your code inside this, pure CSS, no JS required.
+// <pre>
+// &lt; scroll-container &gt;
+// 	&lt; scroll-page &gt;
+// 		...
+// 	&lt; /scroll-page &gt;
+// &lt; /scroll-container &gt;
+// </pre>
