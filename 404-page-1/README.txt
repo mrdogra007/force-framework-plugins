d973b2210e175ab35704b531aa50c58c7741c01c
Nishant Dogra <13938655+mrdogra007@users.noreply.github.com>
2019-06-04 16:00:13 +0530

    404 pages
    404 pages

---
+++ 404-page-1/README.txt
@@ -0,0 +1,4 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/mrdogra007/pen/RdWLMM.
+
+ 404 Page concept, designed for DograsWeblog.com
+Its cool and based on Particle JS
---
+++ 404-page-2/js/index.js
@@ -0,0 +1,12 @@
+// Created: 28-02-2019
+// Author: Nishant Dogra
+// Twitter: @mrdogra007
+// http://dograsweblog.com/drive/force-framework/
+
+var particles;
+particles = Particles.init({
+  selector: ".background",
+  color: ["#da0463", "#3e08a1", "#0c84ff"],
+  connectParticles: true,
+  speed: 1
+});
